987a334abcc58c00f102a6127ad3fd825b6a05a5
Niksimas <yarosniks@gmail.com>
2024-03-23 16:23:52 +0700

    ...

--- core/administrate/__init__.py
+++ core/administrate/__init__.py
@@ -2,13 +2,12 @@ from aiogram import Router, F
 
 from .basic import router as adm
 from .cancel_state import subrouter as cst
-from .add_service import subrouter as add_poj
 from .edit_service import subrouter as edit_poj
 from .edit_message import subrouter as edit_mess
 from core.database.database import get_all_id_admin
 
 router_admin = Router()
-router_admin.include_routers(adm, add_poj, edit_poj, edit_mess)
+router_admin.include_routers(adm, edit_poj, edit_mess)
 # Должен быть последним
 router_admin.include_router(cst)
 
